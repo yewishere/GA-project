e890bd406ebb13e13fc6c99497021e6949ea6092
medo972283 <medo972283@gmail.com>
2017-12-01 15:03:19 +0800

    Update Costco_GA.js

--- config/Costco_GA.js
+++ config/Costco_GA.js
@@ -11,3 +11,10 @@ else {
   CSS_URL = "https://medo972283.github.io/GA-project/config/Costco_GA.css";
   LIB_URL = "https://medo972283.github.io/GA-project/ga_inject_lib.js"
 }
+
+var exec = function(){
+  auto_set_user_id();
+  
+  //腳本
+  ga_mouse_over_event(".menu-trigger","click");
+}

77d4a367597fb2ab541dc1acf4ea25bb76373a89
medo972283 <medo972283@gmail.com>
2017-12-01 11:47:50 +0800

    Update Costco_GA.js

--- config/Costco_GA.js
+++ config/Costco_GA.js
@@ -1,1 +1,13 @@
+GA_TRACE_CODE = "UA-110096492-2";
 
+var _local_debug = false;
+
+if(_local_debug == false){
+  CSS_URL = "https://localhost/GA-project/config/Costco_GA.css";
+  LIB_URL = "https://localhost/GA-project/ga_inject_lib.js"
+  console.log("[LOCAL TEST MODE]");
+}
+else {
+  CSS_URL = "https://medo972283.github.io/GA-project/config/Costco_GA.css";
+  LIB_URL = "https://medo972283.github.io/GA-project/ga_inject_lib.js"
+}

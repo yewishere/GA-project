983508f1232f088e8eeef2d4a23004aeb899d555
medo972283 <medo972283@gmail.com>
2018-01-03 18:20:07 +0800

    Update Costco_GA.js

--- config/Costco_GA.js
+++ config/Costco_GA.js
@@ -5,21 +5,31 @@ var _local_debug = true;
 if(_local_debug == false){
   CSS_URL = "https://localhost/GA-project/config/Costco_GA.css";
   LIB_URL = "https://localhost/GA-project/ga_inject_lib.js";
-  //LIB_URL = "https://localhost/GA-project/config/Costco_GA.js";
+  
+  //Costco_GA.js 直接加入在網頁head上
   console.log("[LOCAL TEST MODE]");
 }
 else {
   CSS_URL = "https://medo972283.github.io/GA-project/config/Costco_GA.css";
   LIB_URL = "https://medo972283.github.io/GA-project/ga_inject_lib.js";
-  //LIB_URL = "https://localhost/GA-project/config/Costco_GA.js";
+  
 }
 
 var exec = function(){
   auto_set_user_id();
   
   //腳本
-  ga_mouse_over_event(".menu-trigger","Hover Menu");
-  ga_mouse_click_event(".menu-trigger","Click Menu");
+  ga_mouse_over_event(".menu-trigger","Classification_Menu_Hover");
+  ga_mouse_click_event(".menu-trigger","Classification_Menu_Click");
+  
+  ga_mouse_over_event(".nbc-container","Other_Menu_Hover");
+  ga_mouse_click_event(".nbc-container","Other_Menu_Click");
+  ga_mouse_over_event(".topmenu","Top_Menu_Hover");
+  ga_mouse_click_event(".topmenu","Top_Menu_Click");
+  ga_mouse_over_event("#1-1-0","Sub_Menu_Hover");
+  ga_mouse_click_event("#1-1-0","Sub_Menu_Hover");
+  
+  
 }
 
 

839b7481004a6eef7745e714d5101a311e6cfbb1
medo972283 <medo972283@gmail.com>
2018-01-07 13:11:41 +0800

    Update Costco_GA.js

--- config/Costco_GA.js
+++ config/Costco_GA.js
@@ -77,10 +77,10 @@ $(function() {
     for(var i=0;i<len_topmenu;i++){
         $('#theMenu').find('.topmenu').eq(i).attr('id', 'top_menu'+i);
 	    
-	len_submenu = $('#top_menu'+i).find('ul').length;
+	var len_submenu = $('#top_menu'+i).find('ul').length;
 	for(var k=0;k<len_submenu;k++){
-	    len_subsubmenu = $('#top_menu'+i).find('ul').eq(k).find('li').length;
-	    for(z=0;z<subsubmenu;z++){
+	    var len_subsubmenu = $('#top_menu'+i).find('ul').eq(k).find('li').length;
+	    for(z=0;z<len_subsubmenu;z++){
 	        $('#top_menu'+i).find('ul').eq(k).find('li').eq(z).attr('id', 'subsub_menu'+z);
 	    };
 	};
